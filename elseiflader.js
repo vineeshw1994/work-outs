70cfb13360404f3e07b49a0f6e7495b05cb59baa
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-06-15 21:18:41 +0530

    ElseIfLader

---
+++ elseiflader.js
@@ -0,0 +1,14 @@
+let mark =75
+if(mark>=90){
+   console.log('A')
+}else if(mark>=80){
+    console.log('B')
+}else if(mark>=70){
+    console.log('C')
+}else if(mark>=60){
+    console.log('D')
+} else if(mark>=50){
+    console.log('E')
+} else if (mark<50){
+    console.log('Failed')
+}
