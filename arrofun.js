4a68e21c0a2c36624545995c1e8f14c971a0e5d1
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-06-27 23:24:20 +0530

    marks calculations

---
+++ arrofun.js
@@ -0,0 +1,7 @@
+const W_Test = 55
+const L_Exam = 73
+const Assignment= 87
+
+let grade = ((W_Test*70)/100 +(L_Exam*20)/100 + (Assignment*10)/100)
+
+console.log("Grade of the student is"+ " "+grade)
