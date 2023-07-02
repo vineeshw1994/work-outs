cdef24b9267c4deea3bfd13faea4c7d48d1636df
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-07-02 23:09:44 +0530

    set and map workouts

---
+++ setandmap.js
@@ -0,0 +1,24 @@
+let array = [1,2,3,4,5,6,7,8,9,1,3,5,7,9]
+
+let original = new Set(array)
+//console.log(original);
+
+original.add(4)
+original.add(10)
+original.add('vineesh')
+//console.log(original);
+
+let arr = Array.from(original)
+
+//console.log(arr);
+
+let map1 = new Map()
+ map1.set('a',1)
+ map1.set('b',2)
+
+ //console.log(map1);
+
+ map1.set('c',1)
+ //console.log(map1);
+
+ 
