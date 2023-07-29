ad3bb91eab3a8f808d15fead0500a4c1fc41240f
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-07-29 23:14:59 +0530

    linked list data structure

--- linkedlist.js
+++ linkedlist.js
@@ -1,10 +1,11 @@
-class Node {
-    constructor(data){
-        this.data = data
-        this.next = null
+class node{
+    constructor(value){
+        this.value = value
+        this.null = null
     }
 }
-class Singlist{
+
+class linkedlist{
     constructor(){
         this.head = null
         this.size = 0
@@ -14,86 +15,41 @@ class Singlist{
         return this.size === 0
     }
 
-    size(){
+    getsize(){
         return this.size
     }
 
-    prepend(data){
-        const node = new Node (data)
+    prepend(value){
+        const nod = new node(value)
         if(this.empty()){
-            this.head = node
-        }
-        else{
-            node.next = this.head
-            this.head = node
-        }
-        return this.size++
-    }
-
-    append(data){
-        const node = new Node(data)
-        if(this.empty()){
-            this.head = node
-        }
-        else{
-            var prev = this.head
-            while(prev.next){
-                prev = prev.next
-            }
-            prev.next = node
+            this.head =node
+        } else{
+            nod.next = this.size.head
+            this.head = nod
         }
         this.size++
     }
-
     print(){
         if(this.empty()){
-            console.log("List is empty");
-        }
-        else{
+            console.log('list is empty');
+        } else{
             let curr = this.head
-            var listValues = ''
+            let listvalues = ''
             while(curr){
-                listValues +=`${curr.data} `
+                listvalues += `${curr.value}`
                 curr = curr.next
             }
-            console.log(listValues);
+            console.log(listvalues);
         }
     }
 }
 
-const list = new Singlist()
-// list.print()
-// list.prepend(10)
-// list.prepend(20)
-// list.prepend(30)
-// list.prepend(40)
-// list.prepend(50)
-// list.print()
-// list.append(10)
-// list.append(20)
-// list.append(30)
-// list.append(40)
-// list.append(50)
-// list.print()
-
-
-const array = [1,2,3,4,5]
-
-//console.log(array instanceof Array);
-
-
-const person = {
-    name: 'John',
-    age: 30,
-    address: {
-      street: '123 Main St',
-      city: 'Exampleville',
-      country: 'Exampleland'
-    }
-  };
-
-  console.log(person.address.number);
-
-
-
-  
+const list = new linkedlist()
+console.log('list is empty?',  list.empty())
+console.log('list size',  list.getsize());
+list.print()
+list.prepend(10)
+list.print()
+list.prepend(20)
+list.prepend(30)
+list.print()
