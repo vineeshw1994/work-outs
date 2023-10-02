093b3f151105b974328988538401798d63912ea6
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-10-02 23:22:36 +0530

    even number finding

--- A1.js
+++ A1.js
@@ -1,7 +1,13 @@
 const person =[1,2,3,4,5,6,7,8,9]
 
-for(let person of data){
-  console.log(data);
-}
+// for(let person of data){
+//   console.log(data);
+// }
 
-console.table(person)
+// console.table(person)
+
+for (let i =0 ; i < person.length; i++){
+if(persion[i] % 2 === 0 ){
+  console.log('the even number is',i)
+}
+}
